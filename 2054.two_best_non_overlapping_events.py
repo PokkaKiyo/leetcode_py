e5f8e6276b39b8e094fd92274890fc6568226031
PokkaKiyo <e0030925@u.nus.edu>
2024-12-08 15:37:09 +0800

    2054. Two Best Non-Overlapping Events
    - With a heap

--- 2054.two_best_non_overlapping_events.py
+++ 2054.two_best_non_overlapping_events.py
@@ -1,4 +1,5 @@
 from bisect import bisect_right
+from heapq import heappop, heappush
 from typing import List
 
 
@@ -29,5 +30,27 @@ class Solution:
 
             prev_val = val
             prev_end = end
+
+        return best
+
+    def maxTwoEvents2(self, events: List[List[int]]) -> int:
+        events.sort(key=lambda x: x[0])
+
+        best = 0
+        max_in_front = 0
+        pq: list[tuple[int, int]] = []
+
+        for event in events:
+            start, end, val = event
+            # look in front and see if any end times are earlier than start time
+            while pq and pq[0][0] < start:
+                end_, val_ = heappop(pq)
+                max_in_front = max(max_in_front, val_)
+            best = max(best, max_in_front + val)
+
+            # we only need to push the new event to the stack
+            # all previous events, we only cared about their largest val,
+            # which has been encoded in "max_in_front"
+            heappush(pq, (end, val))
 
         return best

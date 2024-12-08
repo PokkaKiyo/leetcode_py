f6ca71da15a4bf17c25e74adb14de8d5ecc3aaa9
PokkaKiyo <e0030925@u.nus.edu>
2024-12-08 15:18:50 +0800

    2054. Two Best Non-Overlapping Events
    - Optimize, in the case where the next value does not have a greater score or an earlier end time, skip the bisect

--- 2054.two_best_non_overlapping_events.py
+++ 2054.two_best_non_overlapping_events.py
@@ -13,13 +13,21 @@ class Solution:
             maximum.append(best)
 
         best = 0
+        prev_end, prev_val = -1, -1
         for idx, event in enumerate(events):
             _start, end, val = event
+
+            if val <= prev_val and end >= prev_end:
+                continue
+
             right = bisect_right(events, end, idx + 1, key=lambda x: x[0])
             if right >= len(events):
                 solution = 0
             else:
                 solution = maximum[len(events) - 1 - right]
             best = max(best, val + solution)
+
+            prev_val = val
+            prev_end = end
 
         return best

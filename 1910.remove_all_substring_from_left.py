f8e4ed1503511dfa6a600fcb49947479b035fc00
PokkaKiyo <31039465+PokkaKiyo@users.noreply.github.com>
2025-02-11 22:44:23 +0800

    1910. Remove All Occurrences of a Substring

--- 1910.remove_all_substring_from_left.py
+++ 1910.remove_all_substring_from_left.py
@@ -1,4 +1,24 @@
 class Solution:
+    def removeOccurrences(self, s: str, part: str) -> str:
+        if len(part) == 1:
+            front, last_ch = None, part
+        else:
+            front, last_ch = list(part[:-1]), part[-1]
+
+        stack = []
+        for ch in s:
+            if ch != last_ch:
+                stack.append(ch)
+                continue
+            if front is None:
+                continue
+            if stack[-len(front) :] == front:
+                stack = stack[: -len(front)]
+            else:
+                stack.append(ch)
+
+        return "".join(stack)
+
     def removeOccurrences_trivial(self, s: str, part: str) -> str:
         while True:
             left, _, right = s.partition(part)
